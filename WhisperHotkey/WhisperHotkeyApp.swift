ac8dc8754ce0a6160b57a19cccbac96c0bbdb93b
idosofi <ido.sofi@gmail.com>
2025-06-26 15:50:56 +0300

    Initial Commit

---
+++ WhisperHotkey/WhisperHotkeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhisperHotkeyApp.swift
+//  WhisperHotkey
+//
+//  Created by Ido Sofi on 26/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct WhisperHotkeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
